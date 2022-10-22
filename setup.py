31cba46d9bcbbfe7ae5c1319739ac24322afc8ad
中山功太 <nakayamakouta@nakayamakoutanoMacBook-Pro-2.local>
2022-10-22 16:16:41 +0900

    バージョンアップ

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def _requires_from_file(filename):
 
 setup(
     name="roberta_tokenizer",
-    version="1.0.0",
+    version="1.0.1",
     description="RoBERTa Tokenizer",
     author="k141303",
     url="https://github.com/k141303/roberta_tokenizer",
