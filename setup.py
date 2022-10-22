f7fd8aa63f467a54550d909d436e1d8f0e133125
中山功太 <nakayamakouta@nakayamakoutanoMacBook-Pro-2.local>
2022-10-22 16:21:41 +0900

    バグ修正

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ def _requires_from_file(filename):
 
 setup(
     name="roberta_tokenizer",
-    version="1.0.1",
+    version="1.0.2",
     description="RoBERTa Tokenizer",
     author="k141303",
     url="https://github.com/k141303/roberta_tokenizer",
-    packages=["roberta_tokenizer"],
+    packages=["roberta_tokenizer", "roberta_tokenizer.tokenizer"],
     package_dir={"": "src"},
     include_package_data=True,
     install_requires=_requires_from_file('requirements.txt'),
